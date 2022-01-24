92a2dfc2e34b3e3c5d91785192fbb529623525cf
ROCKTAKEY <rocktakey@gmail.com>
2022-01-25 02:12:14 +0900

    docs: Emacs-24.3 should be required.

--- rhq.el
+++ rhq.el
@@ -6,7 +6,7 @@
 ;; Keywords: tools, extensions
 
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.1"))
+;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/ROCKTAKEY/rhq
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
