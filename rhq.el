aa5de9d097b2f4b67d8ff62d25de515095efcea6
ROCKTAKEY <rocktakey@gmail.com>
2022-01-27 03:51:55 +0900

    docs: Update version.

--- rhq.el
+++ rhq.el
@@ -5,7 +5,7 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: tools, extensions
 
-;; Version: 0.1.4
+;; Version: 0.1.5
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/ROCKTAKEY/rhq
 ;; This program is free software; you can redistribute it and/or modify
