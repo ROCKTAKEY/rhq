f87a43a114d72bd0e000827c0a3a88cc34e2a29b
ROCKTAKEY <rocktakey@gmail.com>
2022-01-28 22:57:45 +0900

    test: Remove test.

--- test/rhq-test.el
+++ test/rhq-test.el
@@ -39,9 +39,5 @@
 (ert-deftest rhq--check-executable-availability ()
   (rhq--check-executable-availability))
 
-(ert-deftest rhq-clone ()
-  (rhq-clone "https://github.com/ubnt-intrepid/rhq")
-  (rhq-clone "ROCKTAKEY/rhq"))
-
 (provide 'rhq-test)
 ;;; rhq-test.el ends here
